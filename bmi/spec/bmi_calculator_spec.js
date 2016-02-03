b81fbcbf75231b6028de271a2cef4205ba9fc515
Mazen <mmeezzoo26@me.com>
2016-02-03 21:15:40 +0100

    function to calculate imperical added to #person and #calculation

--- bmi/spec/bmi_calculator_spec.js
+++ bmi/spec/bmi_calculator_spec.js
@@ -1,15 +1,22 @@
 describe("BMICalculator", function() {
   var bmi_calculator;
   var person;
+  var person_2;
 
   beforeEach(function() {
     person = new Person({weight: 90, height: 186});
+    person_2 = new Person({weight: 150, height: 64});
     calculator = new BMICalculator();
   });
 
   it("calculates BMI for a person using metric method", function() {
     calculator.metric_bmi(person);
     expect(person.bmiValue).toEqual(26.01);
+  });
+
+  it("calculates BMI for a person using imperial method", function() {
+    calculator.imperial_bmi(person_2);
+    expect(person_2.bmiValue).toEqual(25.74);
   });
 
 });
--- bmi/spec/person_spec.js
+++ bmi/spec/person_spec.js
@@ -1,8 +1,10 @@
 describe("Person", function() {
   var person;
+  var person_2
 
   beforeEach(function() {
     person = new Person({weight: 90, height: 186});
+    person_2 = new Person({weight: 150, height: 64});
   });
 
   it("should have weight of 90", function() {
@@ -13,13 +15,18 @@ describe("Person", function() {
     expect(person.height).toEqual(186);
   });
 
-  it("should calculate BMI value", function() {
-  person.calculate_bmi();
+  it("should calculate BMI value in metric", function() {
+  person.calculate_bmi_met();
   expect(person.bmiValue).toEqual(26.01)
   });
 
+  it("should calculate BMI value in imperial", function() {
+  person_2.calculate_bmi_imp();
+  expect(person_2.bmiValue).toEqual(25.74)
+  });
+
   it("should have a BMI Message", function() {
-  person.calculate_bmi();
+  person.calculate_bmi_met();
   expect(person.bmiMessage).toEqual("Overweight")
   });
 
