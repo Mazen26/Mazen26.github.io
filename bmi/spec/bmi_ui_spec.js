55e11d7a448f8fab63e2ebd589c6568ad348ba36
Mazen <mmeezzoo26@me.com>
2016-02-04 10:48:20 +0100

    dropdown refactoring / WIP UI-spec to include Imperical

--- bmi/spec/bmi_ui_spec.js
+++ bmi/spec/bmi_ui_spec.js
@@ -1,18 +1,41 @@
 describe('BMI_UI - index.html', function() {
     beforeEach(function() {
-        jasmine.getFixtures().fixturesPath = '.';
-        loadFixtures('index.html');
-        $.holdReady(false);
-        $('#weight').val('90');
-        $('#height').val('186');
-        $('#calculate').trigger('click');
-    });
-    it("displays BMI Value", function() {
-            expect($('#display_value').text()).toBe('Your BMI is 26.01');
-        });
+         jasmine.getFixtures().fixturesPath = '.';
+         loadFixtures('index.html');
+         $.holdReady(false);
 
-    it("displays BMI Message", function() {
-            expect($('#display_message').text()).toBe('and you are Overweight');
-        });
+     describe('Metric measurments', function() {
+         beforeEach(function() {
+           $('#mySelect').val('Metric');
+           $('#weight').val('90');
+           $('#height').val('186');
+           $('#calculate').trigger('click');
+         });
 
+       it("displays BMI Value", function() {
+          expect($('#display_value').text()).toBe('Your BMI is 26.01');
+       });
+
+       it("displays BMI Message", function() {
+         expect($('#display_message').text()).toBe('and you are Overweight');
+       });
+     });
+
+     describe('Imperial measurments', function() {
+         beforeEach(function() {
+           $('#mySelect').val('Imperial');
+           $('#weight').val('150');
+           $('#height').val('64');
+           $('#calculate').trigger('click');
+         });
+
+       it("displays BMI Value", function() {
+          expect($('#display_value').text()).toBe('Your BMI is 25.74');
+       });
+
+       it("displays BMI Message", function() {
+         expect($('#display_message').text()).toBe('and you are Overweight');
+       });
+     });
+  });
 });
